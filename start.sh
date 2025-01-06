0e1b0433a698a15540ebd0780165ec597381d056
Alejandro <alejandrolias84@gmail.com>
2025-01-05 23:26:27 -0500

    problemas con el puerto

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Obtener el puerto asignado por Render o usar 8000 si no está definido
+PORT=${PORT:-8000}
+
 # Ejecutar las migraciones
 php artisan migrate --force
 
-# Iniciar el servidor de Laravel
-php artisan serve
+# Iniciar el servidor de Laravel en el puerto asignado por Render
+php artisan serve --host=0.0.0.0 --port=$PORT
